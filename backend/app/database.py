1497db61fbadd953b25dd627fa50edcd013d448a
MasvsaM <michu23serre@gmail.com>
2025-09-21 17:54:27 -0300

    Cambio Estructura

--- backend/app/database.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URL = "mongodb://localhost:27017"
-client = AsyncIOMotorClient(MONGO_URL)
-db = client.marketpet  # nombre de la base de datos
-mascotas_collection = db.mascotas
-usuarios_collection = db.usuarios
-
