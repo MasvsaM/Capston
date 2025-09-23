300e046af3c294d1d4b1da372c580e3dde7875f4
MasvsaM <michu23serre@gmail.com>
2025-09-23 12:18:11 -0300

    delete

--- Market/Market/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Market',
-  webDir: 'www'
-};
-
-export default config;
