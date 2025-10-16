6335d4c60177069301c7d998a6c6b42a5327b36f
Hutaru <f.vaccaro@duocuc.cl>
2025-10-16 17:23:24 -0300

    prueba

--- Market/src/main.ts
+++ Market/src/main.ts
@@ -10,6 +10,15 @@ import { provideStorage, getStorage } from '@angular/fire/storage';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { environment } from './environments/environment';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getDataConnect, provideDataConnect } from '@angular/fire/data-connect';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
 
 declare global {
   interface Window {
@@ -58,6 +67,10 @@ bootstrapApplication(AppComponent, {
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage()),
+    provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({ projectId: "marketpet-708c9", appId: "1:1053161160027:web:19d7d51f8c64c57fa16e7e", storageBucket: "marketpet-708c9.firebasestorage.app", apiKey: "AIzaSyD3X_8vvI_Zherq77qdTAlqc2xYcpDuuHw", authDomain: "marketpet-708c9.firebaseapp.com", messagingSenderId: "1053161160027", measurementId: "G-0M9WYQPHJB", projectNumber: "1053161160027", version: "2" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise site key here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider('YOUR_RECAPTCHA_ENTERPRISE_SITE_KEY');
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideDataConnect(() => getDataConnect({connector: "example",location: "southamerica-west1",service: "market"})), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI()), provideFirebaseApp(() => initializeApp({ projectId: "marketpet-708c9", appId: "1:1053161160027:web:19d7d51f8c64c57fa16e7e", storageBucket: "marketpet-708c9.firebasestorage.app", apiKey: "AIzaSyD3X_8vvI_Zherq77qdTAlqc2xYcpDuuHw", authDomain: "marketpet-708c9.firebaseapp.com", messagingSenderId: "1053161160027", measurementId: "G-0M9WYQPHJB", projectNumber: "1053161160027", version: "2" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideStorage(() => getStorage()),
   ],
 }).catch(error => console.error(error));
